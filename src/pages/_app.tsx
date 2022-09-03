bdff59547cdfc054eba0228c8f3a8fe28470f032
Zul Ikram Musaddik Rayat <2010050@me.buet.ac.bd>
2022-09-04 02:15:22 +0600

    added google analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,6 +4,7 @@ import { AppShell, MantineProvider } from "@mantine/core";
 import MyHeader from "~/components/common/Header";
 import { RouterTransition } from "~/components/common/RouterTransition";
 import Seo from "~/components/common/Seo";
+import Script from "next/script";
 
 function ExampleApp({ Component, pageProps }: AppProps) {
   return (
@@ -26,6 +27,19 @@ function ExampleApp({ Component, pageProps }: AppProps) {
       <AppShell header={<MyHeader />} fixed={false}>
         <Component {...pageProps} />
       </AppShell>
+      <Script
+        strategy="afterInteractive"
+        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_MEASUREMENT_ID}`}
+      />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', '${process.env.NEXT_PUBLIC_MEASUREMENT_ID}');
+        `}
+      </Script>
     </MantineProvider>
   );
 }
